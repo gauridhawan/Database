ccbcbe1f20029955cfd4a5f19254719c3bded38e
gauridhawan <dhawan.gauri@gmail.com>
2020-11-21 00:14:36 -0500

    added beginRo, W, R transactionType

--- src/DBReader.java
+++ src/DBReader.java
@@ -8,5 +8,29 @@ public class DBReader {
         this.isStdin = isStdin;
     }
 
-
+    public Instruction getNextInstruction(String line){
+        String[] str = line.split("[\\(\\)]");
+        Instruction instr = new Instruction();
+        String transactionType = str[0];
+        if(transactionType.equalsIgnoreCase(TransactionType.begin.getTransactionType()) ||
+                transactionType.equalsIgnoreCase(TransactionType.beginRO.getTransactionType())){
+            instr.transactionType = TransactionType.begin;
+            if(transactionType.equalsIgnoreCase(TransactionType.beginRO.getTransactionType())){
+                instr.transactionType = TransactionType.beginRO;
+            }
+            String[] values = str[1].split(",");
+            instr.transactionId = values[0];
+        }else if(transactionType.equalsIgnoreCase(TransactionType.W.getTransactionType())){
+            instr.transactionType = TransactionType.W;
+            String[] values = str[1].split(",");
+            instr.transactionId = values[0];
+            instr.variableName = values[1];
+            instr.value = Integer.parseInt(values[2]);
+        }else if(transactionType.equalsIgnoreCase(TransactionType.R.getTransactionType())){
+            instr.transactionType = TransactionType.R;
+            String[] values = str[1].split(",");
+            instr.transactionId = values[0];
+            instr.variableName = values[1];
+        }
+    }
 }
--- src/Instruction.java
+++ src/Instruction.java
@@ -1,7 +1,70 @@
 public class Instruction {
 
     TransactionType transactionType;
-    String variable;
+    String variableName;
+    String transactionId;
+    int value;
+    int site;
 
+    Instruction(TransactionType transactionType){
+        this.transactionType = transactionType;
+    }
 
+    Instruction(TransactionType transactionType, String variableName, int value){
+        this.transactionType = transactionType;
+        this.variableName = variableName;
+        this.value = value;
+    }
+
+    Instruction(int site){
+        this.site = site;
+    }
+
+    Instruction(TransactionType type, String id){
+        this.transactionType = type;
+        this.transactionId = id;
+    }
+
+    public Instruction() {
+    }
+
+    public TransactionType getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(TransactionType transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public String getVariableName() {
+        return variableName;
+    }
+
+    public void setVariableName(String variableName) {
+        this.variableName = variableName;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getSite() {
+        return site;
+    }
+
+    public void setSite(int site) {
+        this.site = site;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
 }
