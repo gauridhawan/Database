359a7098f3de16f1c7ba2c4db93d0208f25c9c4b
gauridhawan <dhawan.gauri@gmail.com>
2020-11-22 01:47:43 -0500

    added transaction class

--- src/DBReader.java
+++ src/DBReader.java
@@ -43,6 +43,8 @@ public class DBReader {
             instr.transactionType = TransactionType.dump;
         }
 
+
+
         return instr;
     }
 
---
+++ src/TransactionStatus.java
@@ -0,0 +1,6 @@
+public enum TransactionStatus {
+    RUNNING,
+    WAITING,
+    ABORTED,
+    COMMITTED;
+}
