f9cdaa4aeb4d7073e58363469cd43c8aded3780d
gauridhawan <dhawan.gauri@gmail.com>
2020-11-23 02:27:45 -0500

    added committransaction method

---
+++ src/SiteManager.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+
+public class SiteManager {
+
+    Map<Integer,Site> siteMap;
+
+    public Map<Integer, Site> getSiteMap() {
+        return siteMap;
+    }
+
+    public void setSiteMap(Map<Integer, Site> siteMap) {
+        this.siteMap = siteMap;
+    }
+
+}
