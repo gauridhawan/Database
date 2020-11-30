6e0b0a0a62ea66dd353fd8b695e511364b5f0c1d
gauridhawan <dhawan.gauri@gmail.com>
2020-11-29 23:17:37 -0500

    updated test cases

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) throws FileNotFoundException {
-        for (int t = 15; t<=15;t++) {
+        for (int t = 1; t<=1;t++) {
             System.out.println("###### TEST "+ t +" ###########");
             SiteManager siteManager = new SiteManager(10, 20);
             TransactionManager transactionManager = new TransactionManager();
             DBReader dbReader = new DBReader("../testcases/test2.txt", false);
             int time = 0;
-            File file = new File("/Users/kunalkhatri/Desktop/Semester3/ADB/Porject/Database/testcases/" +
-                    "test_cases/test" + t);
+            File file = new File("/Users/geetadhawan/Documents/GitHub/Database/testcases/" +
+                    "test_script_3_7" +".txt");
 
             Scanner fileReader = new Scanner(file);
             while (fileReader.hasNextLine()) {
